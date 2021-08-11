61e1c4b13c84765bd181e807052534acc60815cb
jack <78677992+j-a-c-k-goes@users.noreply.github.com>
2021-08-12 09:26:06 +1200

    Update hex_colors.py
    shortened code by splitting main function into two separate functions — one to print the hex, the other, to make the hex; included try/except statements

--- hex_colors.py
+++ hex_colors.py
@@ -2,37 +2,29 @@
 Take input as number
 Output hex codes up to number of requested colors
 """
-
-# ............................................................ Imports
 from random import *
 import pyperclip
-# ............................................................ Function
-def make_hex_code(number_of_colors = 3):
+import math
 
-	hex_codes = []
-
-	digits = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
-
-	for i in range(number_of_colors):
-		hex_code = '#%s%s%s%s%s%s' % (choice(digits), choice(digits), choice(digits), choice(digits), choice(digits), choice(digits))
-		hex_codes.append(hex_code)
-
-	pyperclip.copy('\n'.join(hex_codes).upper())
-
-	print('%s colors copied to clipboard!'  % (number_of_colors))
-
-	print('\n'.join(hex_codes).upper())
-
-	"\n".join(hex_codes)
-
-	return hex_codes
-# ............................................................ On Run
+def print_hex(n=3):
+        collection = []
+        for i in range(n):
+                collection.append(make_hex_code())
+        print('hex codes made.')
+        print('{} colors copied to clipboard!'.format(len(collection)))
+        collection = '\n'.join(collection).upper()
+        pyperclip.copy(collection)
+        print(collection)
+def make_hex_code():
+        digits = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
+        hex_code = '#{}{}{}{}{}{}'.format(choice(digits), choice(digits), choice(digits), choice(digits), choice(digits), choice(digits))
+        return hex_code
 if __name__ == '__main__':
-
-        number_of_colors = int(input('how many colors will be in your palette? '))
-
-        make_hex_code(number_of_colors)
-
+        try:
+                n = abs(int(input('how many colors will be in your palette? ')))
+        except ValueError:
+                n = abs(int(input('please enter a valid integer: ')))
+        print_hex(n)
 """
 Future updates: detect color for given generated hex code, if #000000, black
 """
