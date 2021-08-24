c9d1ddb0d637c673caa5b1b8dbf4851c19ec624a
jack <78677992+j-a-c-k-goes@users.noreply.github.com>
2021-08-25 04:58:11 +1200

    Update hex_colors.py
    updated conditional error starting on line 42, which handles prefix constraints

--- hex_colors.py
+++ hex_colors.py
@@ -15,41 +15,42 @@ def print_hex(n=3):
         collection = ',\n'.join(collection).lower()
         pyperclip.copy(collection)
         print(collection)
-
+        
+def hex_digits():
+         digits = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
+         return digits
+        
 def make_hex_code():
-        digits = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
-
         if prefix_position == 1:
-                hex_code = '#{}{}{}{}{}'.format(hex_prefix, choice(digits), choice(digits), choice(digits), choice(digits))
+                hex_code = '#{}{}{}{}{}'.format(hex_prefix, choice(hex_digits()), choice(hex_digits()), choice(hex_digits()), choice(hex_digits()))
         elif prefix_position == 2:
-                hex_code = '#{}{}{}{}{}'.format(choice(digits), choice(digits),hex_prefix, choice(digits), choice(digits))
+                hex_code = '#{}{}{}{}{}'.format(choice(hex_digits()), choice(hex_digits()),hex_prefix, choice(hex_digits()), choice(hex_digits()))
         elif prefix_position == 3:
-                hex_code = '#{}{}{}{}{}'.format(choice(digits), choice(digits), choice(digits), choice(digits),hex_prefix)
+                hex_code = '#{}{}{}{}{}'.format(choice(hex_digits()), choice(hex_digits()), choice(hex_digits()), choice(hex_digits()),hex_prefix)
         return hex_code
 
+def menu():
+        print('select a prefix position\n1 front\n2 middle\n3 end')
+        
 if __name__ == '__main__':
         i = 0
         while True:
                 i += 1
-                try:
-                        print('select a prefix position: (1)front (2)middle (3)end')
-                        prefix_position = int(input('> '))
-                        if prefix_position != 1 or prefix_position != 2 or prefix_position != 3:
-                                print('invalid prefix position. use (1), (2), or (3).')
-                                prefix_position = int(input('> '))
-                        print()
+                menu()
+                prefix_position = int(input('> '))
 
+                if prefix_position < 0 or prefix_position > 3 :
+                        print('invlid prefix position. use (1), (2), or (3).')
+                        prefix_position = int(input('> '))
+                        print()
+                        
+                hex_prefix = str(input('enter a 2-character hex prefix (example: a3): '))
+                
+                if len(hex_prefix) != 2:
+                        print('incorrect length. use 2 characters.')
                         hex_prefix = str(input('enter a 2-character hex prefix (example: a3): '))
 
-                        if len(hex_prefix) != 2:
-                                print('incorrect length. use 2 characters.')
-                                hex_prefix = str(input('enter a 2-character hex prefix (example: a3): '))
-
-                        n = abs(int(input('how many colors will be in your palette? ')))
-
-                except ValueError:
-                        n = abs(int(input('please enter a valid integer: ')))
-
+                n = abs(int(input('how many colors will be in your palette? ')))
                 print_hex(n)
                 print()
 
