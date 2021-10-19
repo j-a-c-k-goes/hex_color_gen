c97b36eb29d462acba9ebeda6aaa4d65571eab54
jack <78677992+j-a-c-k-goes@users.noreply.github.com>
2021-10-20 10:50:36 +1300

    Update hex_colors.py

--- hex_colors.py
+++ hex_colors.py
@@ -1,59 +1,37 @@
-"""
-Take input as number
-Output hex codes up to number of requested colors
-"""
 from random import *
 import pyperclip
 import math
-
-def print_hex(n=3):
-        collection = []
-        for i in range(n):
-                collection.append(make_hex_code())
-        print('hex codes made.')
-        print('{} colors copied to clipboard!'.format(len(collection)))
-        collection = ',\n'.join(collection).lower()
-        pyperclip.copy(collection)
-        print(collection)
-        
+colors_base = ['red', 'green', 'blue']
+select_color = str(input('for color genration, enter "red", "green", or "blue": '))
+while select_color not in colors_base:
+        print('use red green or blue to generate colors')
+        select_color = str(input('for color generation, enter "red", "green", or "blue": '))
+number_of_colors = int(input('enter how many color options to generate: '))
+max_hex_value = 'ff'
 def hex_digits():
          digits = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
-         return digits
-        
+         return choice(digits)
 def make_hex_code():
-        if prefix_position == 1:
-                hex_code = '#{}{}{}{}{}'.format(hex_prefix, choice(hex_digits()), choice(hex_digits()), choice(hex_digits()), choice(hex_digits()))
-        elif prefix_position == 2:
-                hex_code = '#{}{}{}{}{}'.format(choice(hex_digits()), choice(hex_digits()),hex_prefix, choice(hex_digits()), choice(hex_digits()))
-        elif prefix_position == 3:
-                hex_code = '#{}{}{}{}{}'.format(choice(hex_digits()), choice(hex_digits()), choice(hex_digits()), choice(hex_digits()),hex_prefix)
+        if select_color == 'red':
+                hex_code = f'{max_hex_value}{hex_digits()}{hex_digits()}{hex_digits()}{hex_digits()}'
+        elif select_color == 'green':
+                hex_code = hex_code = f'{hex_digits()}{hex_digits()}{max_hex_value}{hex_digits()}{hex_digits()}'
+        elif select_color == 'blue':
+                hex_code = hex_code = f'{hex_digits()}{hex_digits()}{hex_digits()}{hex_digits()}{max_hex_value}'
         return hex_code
-
-def menu():
-        print('select a prefix position\n1 front\n2 middle\n3 end')
-        
-if __name__ == '__main__':
-        i = 0
-        while True:
-                i += 1
-                menu()
-                prefix_position = int(input('> '))
-
-                if prefix_position < 0 or prefix_position > 3 :
-                        print('invlid prefix position. use (1), (2), or (3).')
-                        prefix_position = int(input('> '))
-                        print()
-                        
-                hex_prefix = str(input('enter a 2-character hex prefix (example: a3): '))
-                
-                if len(hex_prefix) != 2:
-                        print('incorrect length. use 2 characters.')
-                        hex_prefix = str(input('enter a 2-character hex prefix (example: a3): '))
-
-                n = abs(int(input('how many colors will be in your palette? ')))
-                print_hex(n)
-                print()
-
-                if i > 5:
-                        print('ending program')
-                        break
+def print_hex_values():
+        palette = []
+        if select_color == "red":
+                print('generating red dominant colors')
+        elif select_color == "green":
+                print('generating green dominant colors')
+        elif select_color == "blue":
+                print('generating blue dominant colors')
+        for color in range(number_of_colors):
+                palette.append(make_hex_code())
+        print('hex codes made.')
+        print(f'{len(palette)} colors copied to clipboard!')
+        palette = '\n'.join(palette).upper()
+        pyperclip.copy(palette)
+        print(palette)
+print_hex_values()
